9a1db524df2240b550a45a62c9cb3ea67066c900
Dusan Madjar <madjardusan@gmail.com>
2023-02-05 21:57:27 +0100

    Worked on the form and added shadow to elements as well as animations

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,34 +1,38 @@
 "use strict";
 // import isEmail from "./validator/lib/isEmail.js";
-// import isEmail from "validator/isEmail";
 // const isEmail = require("validator/lib/isEmail");
 // console.log(isEmail("test@example.com")); // true
 
-// form.addEventListener("submit", function (e) {
-//   e.preventDefault();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-//   // Get the user's input from the form
-//   const name = form.querySelector("#name").value;
-//   const email = form.querySelector("#mail").value;
-//   const message = form.querySelector("#message").value;
-//   if (name === "" || email === "" || message == "") {
-//     e.preventDefault();
-//     alert("Enter all data in the form.");
-//   }
-//   console.log("aa");
-//   // Use the Email.js library to send the email
-//   emailjs
-//     .send("service_284tpr9", "template_k9x3scz", {
-//       form_name: name,
-//       email_id: email,
-//       message: message,
-//     })
-//     .then(
-//       function (response) {
-//         console.log("SUCCESS!", response.status, response.text);
-//       },
-//       function (error) {
-//         console.log("FAILED...", error);
-//       }
-//     );
-// });
+  // Get the user's input from the form
+  const name = form.querySelector("#name").value;
+  const email = form.querySelector("#mail").value;
+  const message = form.querySelector("#message").value;
+  if (name === "" || email === "" || message == "") {
+    e.preventDefault();
+    alert("Enter all data in the form.");
+  }
+  console.log("aa");
+  // Use the Email.js library to send the email
+  emailjs
+    .send("service_284tpr9", "template_k9x3sc", {
+      form_name: name,
+      email_id: email,
+      message: message,
+    })
+    .then(
+      function (response) {
+        document.querySelector(".form").style.display = "none";
+        document.querySelector(".successful").style.display = "block";
+      },
+      function (error) {
+        alert(
+          "There were technical problems while sending the message, please contact us directly by email at madjardusan@gmail.com."
+        );
+      }
+    );
+});
+// document.querySelector(".form").style.display = "none";
+// document.querySelector(".successful").style.display = "block";
--- js/script.js
+++ js/script.js
@@ -55,15 +55,24 @@ const observer = new IntersectionObserver(
 observer.observe(header);
 
 ////////////////////////////////////////////
-const links = document.querySelectorAll("nav a");
 
-links.forEach((link) => {
-  link.addEventListener("click", (event) => {
-    event.preventDefault();
-    const targetId = event.currentTarget.getAttribute("href");
-    const targetSection = document.querySelector(targetId);
-    targetSection.scrollIntoView({
-      behavior: "smooth",
-    });
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to the other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
   });
 });
